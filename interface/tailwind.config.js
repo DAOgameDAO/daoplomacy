1bef78501da6a6667cf18081aac1ca609e4c7813
Jannik Luhn <jannik@brainbot.com>
2022-02-12 19:39:26 +0100

    Remove darkmode property

--- interface/tailwind.config.js
+++ interface/tailwind.config.js
@@ -2,7 +2,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false,
   theme: {
     extend: {
       colors: {
